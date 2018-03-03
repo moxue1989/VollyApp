e924a146acd3b399fa66c18a9dcb8d9b2183e3f6
Farsos Bulsara <kingsukhoi@gmail.com>
2018-03-03 11:44:20 -0700

    Added Name start and end time to opportunity

--- Volly/Models/DataModel/Opportunity.cs
+++ Volly/Models/DataModel/Opportunity.cs
@@ -1,7 +1,12 @@
-﻿namespace Volly.Models.DataModel
+﻿using System;
+
+namespace Volly.Models.DataModel
 {
     public class Opportunity
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StarTime { get; set; }
+        public DateTime EndTime { get; set; }
     }
 }
