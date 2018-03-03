b008b1e2452aa4813b56d75986896916027ffaa2
Farsos Bulsara <kingsukhoi@gmail.com>
2018-03-03 11:41:37 -0700

    Added Skills and Avability to user class

--- Volly/Models/DataModel/User.cs
+++ Volly/Models/DataModel/User.cs
@@ -1,8 +1,13 @@
-﻿namespace Volly.Models.DataModel
+﻿using System.Collections.Generic;
+
+namespace Volly.Models.DataModel
 {
     public class User
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<string> Skills{get;set;}
+        public List<string> Avalability { get; set; }
+        
     }
 }
